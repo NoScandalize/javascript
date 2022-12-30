86d7ca64e1ade9e07aade2422d9ad84decd9cfdd
Douglas Wesley <douglas.almeida55@yahoo.com.br>
2022-12-30 19:09:08 -0300

    Update ex008-9-10MD.11
    - Add folder ex008
    - Add folder ex009
    - Add folder ex010
    - Add ex008.js on ex008
    - Add ex009.jet on ex009
    - Add index.html on ex010
    - Add index2.html on ex010

---
+++ Exercicios/ex008/ex008.js
@@ -0,0 +1,6 @@
+var vel = 59
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {  // Condição Simples
+    console.log(`Você ultrapassou a velociade permitida. MUTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
