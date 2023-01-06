086164eea63484aeaad7346c681955baebb43c5a
Douglas Wesley <douglas.almeida55@yahoo.com.br>
2023-01-05 21:21:42 -0300

    Update ex019.ME.6
    - Add ex019

--- Exercicios/ex019/script.js
+++ Exercicios/ex019/script.js
@@ -1,3 +1,18 @@
 function gerar() {
-    
+    var num = document.getElementById('txtnum')
+    var tab = document.getElementById('res')
+    if (num.value.length == 0) {
+        alert('Por favor, digite um número!')
+    } else {
+        var n = Number(num.value)
+        var c = 1
+        res.innerHTML = ''
+        while (c <= 10) {
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.valeu = `tab${c}`
+            res.appendChild(item)
+            c++
+        }
+    }
 }
